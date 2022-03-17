b7beae4866476f1fbb34f201a9a6867a0c202c1e
Baljeet <baljeet@totalpresent.com>
2022-03-17 17:24:11 +0530

    Ui modifications

--- app/src/main/java/com/example/sharedsecuritysystem/UpdateContactActivity.java
+++ app/src/main/java/com/example/sharedsecuritysystem/UpdateContactActivity.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.sharedsecuritysystem.contact_adapter.ContactAdapter;
 
 public class UpdateContactActivity extends AppCompatActivity {
 
     String name, email, phone;
+    EditText editTextName, editTextEmail, editTextPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_contact);
-        Intent intent = new Intent(UpdateContactActivity.this, ContactAdapter.class);
-        name = intent.getStringExtra("Name");
-//        email = getIntent().getStringExtra("Email");
-//        phone = getIntent().getStringExtra("Phone");
 
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("Name");
+        email = intent.getStringExtra("Email");
+        phone = intent.getStringExtra("Phone");
+       // Log.e("check",name);
+        editTextName.setText(name);
+        editTextEmail.setText(email);
+        editTextPhone.setText(phone);
     }
 }
--- app/src/main/java/com/example/sharedsecuritysystem/homeactivity.java
+++ app/src/main/java/com/example/sharedsecuritysystem/homeactivity.java
@@ -1,21 +1,27 @@
 package com.example.sharedsecuritysystem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ImageView imageView;
     DrawerLayout drawerLayout;
     FloatingActionButton floatingActionButton;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,9 @@ public class HomeActivity extends AppCompatActivity {
         imageView = findViewById(R.id.menu);
         drawerLayout=findViewById(R.id.drawer1);
         floatingActionButton = findViewById(R.id.btn_floating);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,7 +43,6 @@ public class HomeActivity extends AppCompatActivity {
                 else{
                     drawerLayout.openDrawer(GravityCompat.START);
                 }
-
             }
         });
 
@@ -45,5 +53,37 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_my_profile:
+                startActivity(new Intent(this, HomeActivity.class));
+//                finishAffinity();
+                break;
+            case R.id.nav_contact_list:
+                startActivity(new Intent(this, ContactListActivity.class));
+//                finishAffinity();
+                break;
+            case R.id.nav_logout:
+                new AlertDialog.Builder(this)
+                        .setTitle("Signing out")
+                        .setMessage("Are you sure you want to sign out?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                                finishAffinity();
+                            }
+                        }).setNegativeButton("No", null).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
