1bc290627751f11f622ba3ac898afa3bb9a2e050
Baljeet <baljeet@totalpresent.com>
2022-03-24 18:09:02 +0530

    ui modify

--- app/src/main/java/com/example/sharedsecuritysystem/LoginActivity.java
+++ app/src/main/java/com/example/sharedsecuritysystem/LoginActivity.java
@@ -1,22 +1,44 @@
 package com.example.sharedsecuritysystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    EditText inputEmail, inputPassword, inputConfirmPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputEmail = findViewById(R.id.textEmailLogin);
+        inputPassword=findViewById(R.id.textPasswordLogin);
+        progressDialog= new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
 
         textView = findViewById(R.id.newuser);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +53,48 @@ public class LoginActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finishAffinity();
+                perForLogin();
+//                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//                finishAffinity();
             }
         });
+    }
+
+    private void perForLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Enter Correct Email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Enter proper password");
+        } else {
+            progressDialog.setMessage("Please wait while Login...");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(LoginActivity.this, TestActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/sharedsecuritysystem/RegistrationActivity.java
+++ app/src/main/java/com/example/sharedsecuritysystem/RegistrationActivity.java
@@ -1,21 +1,44 @@
 package com.example.sharedsecuritysystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
     TextView textView;
     Button button;
+    EditText inputEmail, inputPassword, inputConfirmPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resigtration);
+
+        inputEmail = findViewById(R.id.editTextTextEmailAddress4);
+        inputPassword=findViewById(R.id.editTextTextPassword);
+        inputConfirmPassword=findViewById(R.id.editTextTextConfirmPassword);
+        progressDialog= new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
 
         textView = findViewById(R.id.textview3);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +54,56 @@ public class RegistrationActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
-                startActivity(intent);
-                finishAffinity();
+                PerforAuth();
+//                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//                finishAffinity();
             }
         });
+    }
+
+    private void PerforAuth() {
+        String email=inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
+
+        if(!email.matches(emailPattern))
+        {
+            inputEmail.setError("Enter Correct Email");
+        }else if(password.isEmpty() || password.length()<6)
+        {
+            inputPassword.setError("Enter proper password");
+        }else if(!password.equals(confirmPassword))
+        {
+            inputConfirmPassword.setError("Password not matched");
+        }else
+        {
+            progressDialog.setMessage("Please wait while registration...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegistrationActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity(){
+        Intent intent = new Intent(RegistrationActivity.this, TestActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
